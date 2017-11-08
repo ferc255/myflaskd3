0fa679d1cdde6cea7dd939f5cf3258e9314084f7
ferc255 <alekssslutskiy@yandex.ru>
2017-11-08 09:41:51 +0400

    New files structure

---
+++ myflaskd3/main_app.py
@@ -0,0 +1,15 @@
+"""
+Simple flask application.
+"""
+from flask import Flask
+APP = Flask(__name__)
+
+
+@APP.route('/')
+def sample_view():
+    """
+    Sample view.
+    """
+
+    
+    return 'hello world!'
