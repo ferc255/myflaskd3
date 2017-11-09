3bac69d9f5e1fbbfe95dee104f6e36a8d1fa9706
ferc255 <alekssslutskiy@yandex.ru>
2017-11-09 21:04:19 +0400

    Graph without labels has been done

--- myflaskd3/main_app.py
+++ myflaskd3/main_app.py
@@ -1,8 +1,31 @@
 """
 Simple flask application.
 """
-from flask import Flask
+
+import os
+import json
+from flask import Flask, render_template, send_file
+
 APP = Flask(__name__)
+SOME_JSON = {
+    "nodes": [
+        {"name": "Computer_A1", "kind": "Host"},
+        {"name": "Computer_A2", "kind": "Host"},
+        {"name": "Switch_1", "kind": "Switch"},
+        {"name": "Router_1", "kind": "Router"},
+        {"name": "Switch_2", "kind": "Switch"},
+        {"name": "Computer_B1", "kind": "Host"},
+        {"name": "Computer_B2", "kind": "Host"},
+    ],
+    "links": [
+        {"source": "Computer_A1", "target": "Switch_1", "wire": "Straight"},
+        {"source": "Computer_A2", "target": "Switch_1", "wire": "Straight"},
+        {"source": "Switch_1", "target": "Router_1", "wire": "Straight"},
+        {"source": "Computer_B1", "target": "Switch_2", "wire": "Straight"},
+        {"source": "Computer_B2", "target": "Switch_2", "wire": "Straight"},
+        {"source": "Switch_2", "target": "Router_1", "wire": "Straight"},
+    ],
+}
 
 
 @APP.route('/')
@@ -11,4 +34,23 @@ def sample_view():
     Sample view.
     """
 
-    return 'hello world!'
+    return render_template('graph_sample.html', myjson2=json.dumps(SOME_JSON))
+
+
+@APP.route('/static/<filepath>')
+def static_view(filepath):
+    '''
+    Static view.
+    '''
+
+    return APP.send_static_file(filepath)
+
+
+@APP.route('/media/<filepath>')
+def media_view(filepath):
+    '''
+    Media view.
+    '''
+
+    return send_file(os.path.join(
+        os.path.join(os.getcwd(), 'media'), filepath))
--- set_paths.sh
+++ set_paths.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Just ignore this file. It's only for development.
 . ./venv/bin/activate
-export FLASK_APP=myflaskd3/main_app.py
+export FLASK_APP=main_app.py
 export FLASK_DEBUG=1
