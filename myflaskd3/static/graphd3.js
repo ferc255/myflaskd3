2af0385ba59b05ed9e803b67194ab887b186f1ba
ferc255 <alekssslutskiy@yandex.ru>
2017-11-12 09:42:08 +0400

    Database usage (dirty draft)

--- myflaskd3/static/graphd3.js
+++ myflaskd3/static/graphd3.js
@@ -1,94 +1,32 @@
 var WINDOW_OFFSET = 50;
 var IMAGE_SIZE = 50;
+var MENU_WIDTH = 200;
 
-var svg = d3.select("svg")
-    .attr("width", window.innerWidth)
-    .attr("height", window.innerHeight);
+var svg = d3.select('svg')
+    .attr('width', window.innerWidth)
+    .attr('height', window.innerHeight);
 
-var width = +svg.attr("width");
-var height = +svg.attr("height");
+
+var width = +svg.attr('width');
+var height = +svg.attr('height');
 
 var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) { return d.name; })
+    .force('link', d3.forceLink().id(function(d) { return d.name; })
            .strength(1)
            .distance(100))
-    .force("charge", d3.forceManyBody().strength(-300))
-    .force("center", d3.forceCenter(width / 2, height / 2));
+    .force('charge', d3.forceManyBody().strength(-80))
+    .force('center', d3.forceCenter(width / 2, height / 2));
 
-graph = JSON.parse(myjson);
-
-var link = svg.append("g")
-    .attr("class", "links")
-    .selectAll("line")
-    .data(graph.links)
-    .enter().append("line")
-    .attr("stroke-width", function(d) { return 5; })
-    .attr("stroke", "black")
-    .attr("stroke-dasharray", function(d)
-    {
-        if (d.wire == "Straight")
-        {
-            return "0";
-        }
-        else if (d.wire == "Crossover")
-        {
-            return "15, 5";
-        }
-    });
-
-var node = svg.append("g")
-    .attr("class", "nodes")
-    .selectAll("g")
-    .data(graph.nodes)
-    .enter().append("g");
-
-var vertices = node.append("image")
-    .attr("height", IMAGE_SIZE + "px")
-    .attr("width", IMAGE_SIZE + "px")
-    .attr("href", function(d)
-    {
-        if (d.kind == "Host")
-        {
-            return 'media/computer.png';
-        }
-        else if (d.kind == "Switch")
-        {
-            return 'media/switch.png';
-        }
-        else if (d.kind == "Router")
-        {
-            return 'media/router.png'
-        }
-    })
-    .call(d3.drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended));
-
-labels = node.append("text")
-    .text(function(d)
-    {
-        return d.name;
-    })
-    .attr("stroke-width", 0.3)
-    .attr("stroke", "black")
-    .attr("fill", "#EB5900");
-
-simulation
-    .nodes(graph.nodes)
-    .on("tick", ticked)
-
-simulation.force("link")
-    .links(graph.links);
+var vertices, links, labels, graph_list, menu;
 
 
 function bound(val, type)
 {
-    if (type == "horiz")
+    if (type == 'horiz')
     {
         val = Math.min(val, width - WINDOW_OFFSET);
     }
-    else if (type == "vertic")
+    else if (type == 'vertic')
     {
         val = Math.min(val, height - WINDOW_OFFSET);
     }
@@ -99,28 +37,27 @@ function bound(val, type)
 
 function ticked()
 {
-    console.log('ticked');
     vertices
-        .attr("x", function(d)
+        .attr('x', function(d)
         {
-            d.x = bound(d.x, "horiz");
+            d.x = bound(d.x, 'horiz');
             return d.x - Math.floor(IMAGE_SIZE / 2);
         })
-        .attr("y", function(d)
+        .attr('y', function(d)
         {
-            d.y = bound(d.y, "vertic");
+            d.y = bound(d.y, 'vertic');
             return d.y - Math.floor(IMAGE_SIZE / 2);
         });
 
-    link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+    links
+        .attr('x1', function(d) { return d.source.x; })
+        .attr('y1', function(d) { return d.source.y; })
+        .attr('x2', function(d) { return d.target.x; })
+        .attr('y2', function(d) { return d.target.y; });
     
     labels
-        .attr("x", function(d) { return d.x - Math.floor(IMAGE_SIZE / 2); })
-        .attr("y", function(d) { return d.y - Math.floor(IMAGE_SIZE / 2); });
+        .attr('x', function(d) { return d.x - Math.floor(IMAGE_SIZE / 2); })
+        .attr('y', function(d) { return d.y - Math.floor(IMAGE_SIZE / 2); });
 }
 
 
@@ -147,13 +84,145 @@ function dragended(d)
 }
 
 
-window.addEventListener("resize", function()
+window.addEventListener('resize', function()
 {
-    svg.attr("height", window.innerHeight);
-    svg.attr("width", window.innerWidth);
-    height = +svg.attr("height");
-    width = +svg.attr("width");
+    svg.attr('height', window.innerHeight);
+    svg.attr('width', window.innerWidth);
+    height = +svg.attr('height');
+    width = +svg.attr('width');
     simulation
-        .force("center", d3.forceCenter(width / 2, height / 2));
+        .force('center', d3.forceCenter(width / 2, height / 2));
     simulation.alpha(1).restart();
 });
+
+
+function visualize(graph)
+{
+    graph_list.remove();
+
+    links = svg.append('g')
+        .attr('class', 'links')
+        .selectAll('line')
+        .data(graph.links)
+        .enter().append('line')
+        .attr('stroke-width', function(d) { return 5; })
+        .attr('stroke', 'black')
+        .attr('stroke-dasharray', function(d)
+              {
+                  if (d.wire == 'Straight')
+                  {
+                      return '0';
+                  }
+                  else if (d.wire == 'Crossover')
+                  {
+                      return '15, 5';
+                  }
+              });
+
+    nodes = svg.append('g')
+        .attr('class', 'nodes')
+        .selectAll('g')
+        .data(graph.nodes)
+        .enter()//.append('g');
+
+    
+    labels = nodes.append('text')
+        .text(function(d)
+              {
+                  return d.name;
+              })
+        .attr('stroke-width', 0.3)
+        .attr('stroke', 'black')
+        .attr('fill', '#EB5900');
+    
+    vertices = nodes.append('image')
+        .attr('height', IMAGE_SIZE + 'px')
+        .attr('width', IMAGE_SIZE + 'px')
+        .attr('href', function(d)
+              {
+                  if (d.kind == 'Host')
+                  {
+                      return 'media/computer.png';
+                  }
+                  else if (d.kind == 'Switch')
+                  {
+                      return 'media/switch.png';
+                  }
+                  else if (d.kind == 'Router')
+                  {
+                      return 'media/router.png'
+                  }
+              })
+        .call(d3.drag()
+              .on('start', dragstarted)
+              .on('drag', dragged)
+              .on('end', dragended));
+
+
+    menu = d3.select('body').append('img')
+        .attr('id', 'menu')
+        .attr('height', '50px')
+        .attr('width', '50px')
+        .attr('x', 50)
+        .attr('y', 50)
+        .attr('src', '/media/menu.png')
+        .on('click', start_app)
+
+
+    simulation
+        .nodes(graph.nodes)
+        .on('tick', ticked)
+
+    simulation.force('link')
+        .links(graph.links);
+}
+
+
+function get_graph(address)
+{
+    $.ajax(
+    {
+        url: '/graph/' + address,
+    })
+    .done(function(res)
+    {
+        visualize(res);
+        //console.log(res);
+    });
+}
+
+
+function start_app()
+{
+    try
+    {
+        vertices.remove();
+        labels.remove();
+        nodes.remove();
+        links.remove();
+        menu.remove();
+    }
+    catch (TypeError){}
+    
+    $.ajax(
+    {
+        url: '/graph',
+    })
+    .done(function(res)
+    {
+        graph_list = d3.select('body').append('div')
+            .attr('id', 'divmenu')
+            .style('left', width / 2 - MENU_WIDTH / 2 + "px")
+            .selectAll('div')
+            .data(res['graphs'])
+            .enter().append('div')
+            .attr('class', 'menu_elem')
+            .on('click', function(d)
+            {
+                return get_graph(d);
+            })
+            .text(function(d){return d;});
+    });
+}
+
+start_app();
--- myflaskd3/views.py
+++ myflaskd3/views.py
@@ -4,12 +4,13 @@ Views for the application.
 
 import os
 import json
-from flask import render_template, send_file
+import sqlite3
+from flask import render_template, send_file, jsonify
 
 from myflaskd3 import APP
 
 BASE_DIR = os.path.dirname(__file__)
-SOME_JSON = {
+SOME_JSON1 = {
     "nodes": [
         {"name": "Computer_A1", "kind": "Host"},
         {"name": "Computer_A2", "kind": "Host"},
@@ -63,14 +64,100 @@ SOME_JSON = {
     ],
 }
 
+SOME_JSON2 = {
+    "nodes": [
+        {"name": "Computer_A1", "kind": "Host"},
+        {"name": "Computer_A2", "kind": "Host"},
+        {"name": "Computer_A3", "kind": "Host"},
+        {"name": "Switch_1", "kind": "Switch"},
+        {"name": "Router_1", "kind": "Router"},
+        {"name": "Computer_B1", "kind": "Host"},
+        {"name": "Computer_B2", "kind": "Host"},
+        {"name": "Computer_B3", "kind": "Host"},
+        {"name": "Computer_B4", "kind": "Host"},
+        {"name": "Switch_2", "kind": "Switch"},
+        {"name": "Router_2", "kind": "Router"},
+        {"name": "Computer_C1", "kind": "Host"},
+        {"name": "Computer_C2", "kind": "Host"},
+        {"name": "Computer_C3", "kind": "Host"},
+        {"name": "Switch_3", "kind": "Switch"},
+        {"name": "Router_3", "kind": "Router"},
+    ],
+    "links": [
+        {"source": "Computer_A1", "target": "Switch_1", "wire": "Straight"},
+        {"source": "Computer_A2", "target": "Switch_1", "wire": "Straight"},
+        {"source": "Computer_A3", "target": "Switch_1", "wire": "Straight"},
+        {"source": "Switch_1", "target": "Router_1", "wire": "Straight"},
+        {"source": "Computer_B1", "target": "Switch_2", "wire": "Straight"},
+        {"source": "Computer_B2", "target": "Switch_2", "wire": "Straight"},
+        {"source": "Computer_B3", "target": "Switch_2", "wire": "Straight"},
+        {"source": "Computer_B4", "target": "Switch_2", "wire": "Straight"},
+        {"source": "Switch_2", "target": "Router_2", "wire": "Straight"},
+        {"source": "Computer_C1", "target": "Switch_3", "wire": "Straight"},
+        {"source": "Computer_C2", "target": "Switch_3", "wire": "Straight"},
+        {"source": "Computer_C3", "target": "Switch_3", "wire": "Straight"},
+        {"source": "Switch_3", "target": "Router_3", "wire": "Straight"},
+        {"source": "Router_1", "target": "Router_2", "wire": "Crossover"},
+        {"source": "Router_1", "target": "Router_3", "wire": "Crossover"},
+        {"source": "Router_2", "target": "Router_3", "wire": "Crossover"},
+    ],
+}
 
-@APP.route('/')
+@APP.route('/trash')
+def client_view():
+    conn = sqlite3.connect('database.db')
+    #conn.execute("create table student ("
+    #             "name TEXT, city TEXT)")
+
+    ss1 = json.dumps(SOME_JSON1)
+    ss2 = json.dumps(SOME_JSON2)
+    conn.cursor().execute("insert into Graph (name, json) values"
+                          "('ss1', ?);",
+                          (ss1,) )
+    conn.cursor().execute("insert into Graph (name, json) values"
+                          "('ss2', ?);",
+                          (ss2,) )
+    conn.commit()
+    conn.close()
+    return 'hello'
+
+
+@APP.route('/graph')
+def graph_view():
+    conn = sqlite3.connect('database.db').cursor()
+
+    conn.execute("select name from Graph")
+
+    arr = conn.fetchall();
+    
+    response = {'graphs': []}
+    for item in arr:
+        response['graphs'].append(item[0])
+
+    print(response)
+    
+    return jsonify(response)
+
+
+@APP.route('/graph/<name>')
+def send_graph(name):
+    print(name)
+    db = sqlite3.connect('database.db').cursor()
+    db.execute("SELECT json FROM Graph "
+               "WHERE name = ?;",
+               (name, ) )
+
+    response = db.fetchall()[0][0];
+    return jsonify(json.loads(response))
+
+
+@APP.route('/client')
 def main_view():
     """
     Main view that contains the graph.
     """
 
-    return render_template('graph_sample.html', myjson2=json.dumps(SOME_JSON))
+    return render_template('graph_sample.html')
 
 
 @APP.route('/static/<filepath>')
