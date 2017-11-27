3c1d0c0b3fb24f77546d2c6b84bcdec78b9d7e17
ferc255 <alekssslutskiy@yandex.ru>
2017-11-27 16:29:13 +0400

    Code cleanup

--- myflaskd3/__init__.py
+++ myflaskd3/__init__.py
@@ -6,12 +6,12 @@ from myflaskd3.graph_bp import GRAPH_BP
 
 
 APP = Flask(__name__)
-APP.register_blueprint(GRAPH_BP)    
+APP.register_blueprint(GRAPH_BP)
 
 
 def main():
     """
     The main function which runs server.
     """
-    
+
     APP.run()
--- tests/test_myflaskd3.py
+++ tests/test_myflaskd3.py
@@ -1,20 +1,46 @@
-import myflaskd3
+'''
+Application unit tests
+'''
+
 import unittest
+import myflaskd3
+
 
 class FlaskrTestCase(unittest.TestCase):
+    '''
+    Service class unittest's library
+    '''
+
     def setUp(self):
+        '''
+        Initialization
+        '''
         self.app = myflaskd3.APP.test_client()
 
-        
     def tearDown(self):
+        '''
+        The last actions
+        '''
+
         pass
 
-    
     def test_404(self):
-        vm = self.app.get('/')
-        print(vm.data)
-        assert(b'404 Not Found' in vm.data)
-        
+        '''
+        Check if the server returns 404 for '/' route.
+        '''
+
+        response = self.app.get('/')
+        assert b'404 Not Found' in response.data
+
+    def test_graph_response(self):
+        '''
+        Check if the server returns 200 for '/graph' route.
+        '''
+
+        response = self.app.get('/graph',
+                                headers={'Content-Type': 'text/html'})
+        assert response.status_code == 200
+
 
 if __name__ == '__main__':
     unittest.main()
